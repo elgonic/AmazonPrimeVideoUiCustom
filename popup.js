7ca29fa6b366fa08ed24400ef121e922cb2e04bb
myLaptop <ryo.227069@gmail.com>
2023-05-02 13:19:10 +0900

    初期設定 をONに！

--- popup.js
+++ popup.js
@@ -4,19 +4,30 @@ window.onload = () => {
   const sliderAboutHidePlayButton = document.querySelector("#switch3");
   //Check local storage data
   chrome.storage.local.get(null).then((configData) => {
+
+    console.log(Object.keys(configData));
     console.log(configData["isActivate"]);
     console.log(sliderAboutActivate.querySelector("input").checked);
     console.log(sliderAboutActivate.querySelector("span").textContent);
 
-    sliderAboutActivate.querySelector("input").checked =
-      configData["isActivate"];
-    sliderAboutActivate.querySelector("span").textContent =
-      sliderAboutActivate.querySelector("input").checked ? "ON" : "OFF";
+    //ほんとの最初
+    if (!Object.keys(configData).length) {
+      chrome.storage.local.set({ ["isActivate"]: sliderAboutActivate.querySelector("input").checked});
+      chrome.storage.local.set({ ["isPreventDarkening"]: sliderAboutPreventDarkening.querySelector("input").checked });
+      chrome.storage.local.set({ ["isHidePlaypauseButton"]: sliderAboutHidePlayButton.querySelector("input").checked });
+    } else {
+      sliderAboutActivate.querySelector("input").checked =
+        configData["isActivate"];
+      sliderAboutActivate.querySelector("span").textContent =
+        sliderAboutActivate.querySelector("input").checked ? "ON" : "OFF";
 
-    sliderAboutPreventDarkening.querySelector("input").checked =
-      configData["isPreventDarkening"];
-    sliderAboutHidePlayButton.querySelector("input").checked =
-      configData["isHidePlaypauseButton"];
+      sliderAboutPreventDarkening.querySelector("input").checked =
+        configData["isPreventDarkening"];
+      sliderAboutHidePlayButton.querySelector("input").checked =
+        configData["isHidePlaypauseButton"];
+    };
+
+
 
 
     //UI 初期設定
@@ -53,22 +64,22 @@ window.onload = () => {
   });
 };
 
-const controlCheckBoxes= (masterCheckboxStatus) => {
+const controlCheckBoxes = (masterCheckboxStatus) => {
   const checkBoxes = document.querySelectorAll(".switch_label")
-    let i = 0;
-    checkBoxes.forEach(checkBox => {
-      if(i===0) {
-        i++;
-        return ;
-      } 
-      console.log({masterCheckboxStatus})
-      if(masterCheckboxStatus) {
-        checkBox.querySelector(".circle").classList.remove("circle-disActivate");
-        checkBox.querySelector(".base").classList.remove("base-disActivate");
-      }else{
-        checkBox.querySelector(".circle").classList.add("circle-disActivate");
-        checkBox.querySelector(".base").classList.add("base-disActivate");
+  let i = 0;
+  checkBoxes.forEach(checkBox => {
+    if (i === 0) {
+      i++;
+      return;
+    }
+    console.log({ masterCheckboxStatus })
+    if (masterCheckboxStatus) {
+      checkBox.querySelector(".circle").classList.remove("circle-disActivate");
+      checkBox.querySelector(".base").classList.remove("base-disActivate");
+    } else {
+      checkBox.querySelector(".circle").classList.add("circle-disActivate");
+      checkBox.querySelector(".base").classList.add("base-disActivate");
 
-      }
-    })
+    }
+  })
 }
