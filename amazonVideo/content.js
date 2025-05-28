551c86c8a50d884f85faff28720ecccfa8d95eb1
ssrs <ryo.227069@gmail.com>
2025-05-29 00:03:31 +0900

    PrimeVideoを開いたときにリロードしないと動作しないバグの修正

--- amazonVideo/content.js
+++ amazonVideo/content.js
@@ -48,10 +48,11 @@ const observer = new MutationObserver(records => {
 
     console.log(`isActivate = ${configData["isActivate"]}`);
 
-    records.forEach(record => {
-      record.addedNodes.forEach(node => {
-        if (node.nodeType === Node.ELEMENT_NODE) {
-          const parentNode = node.parentElement;
+//    records.forEach(record => {
+//      record.addedNodes.forEach(node => {
+//        if (node.nodeType === Node.ELEMENT_NODE) {
+          //const parentNode = node.parentElement;
+          const parentNode = document;
           const darkingElements = parentNode.querySelectorAll(".f8hspre.f1makowq");
           const playStopButtons = parentNode.querySelectorAll(".f1aiijcp.fw80uk2");
           const loadButton = parentNode.querySelectorAll(".f1dd7ldt");
@@ -88,34 +89,35 @@ const observer = new MutationObserver(records => {
           } else {
             showElements(titles);
           }
-        }
-      });
+        });
     });
-  });
-});
+//    });
+//  });
+//});
 
 const waitLoadPage = () => {
   console.log("Start CustomUI");
     //なんとなく中央の再生ボタンが配置されてればページ読み込めているといるだろうという希望的観測
-  const terget = document.querySelector(".fq2lkuj");
-  if (terget) {
-    observer.observe(terget , {
-      childList:true,
-      subtree:true
+  const terget = document.querySelector(".dv-player-fullscreen");
+  console.log(terget);
+  if (terget != null) {
+    observer.observe(terget, {
+      childList: true,
+      subtree: true
     });
-  clearInterval(intervalID);
+    clearInterval(intervalID);
   }
-  else{
-      // if now page is not amazon video 
-    numberOfRepetitions++;
-    if (numberOfRepetitions >= repetitionsLimit) {
-      console.log("ClearInterval");
-      clearInterval(intervalID);
-    }
-  }
+//  else {
+//      // if now page is not amazon video 
+//    numberOfRepetitions++;
+//    if (numberOfRepetitions >= repetitionsLimit) {
+//      console.log("ClearInterval");
+//      clearInterval(intervalID);
+//    }
+//  }
 }
 
 let numberOfRepetitions = 0;
-const repetitionsLimit = 10;
+//const repetitionsLimit = 10000;
 let intervalID;
 if (!intervalID) intervalID = setInterval(waitLoadPage, 1000);
