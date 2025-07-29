eb60d4d4da21f5cb17c98ec551d6b4e976001d4e
ssrs <ryo.227069@gmail.com>
2025-07-30 01:37:55 +0900

    字幕設定描画酒精 , 字幕位置修正

--- amazonVideo/content.js
+++ amazonVideo/content.js
@@ -20,21 +20,28 @@ const firstSetup = () => {
 
 const hideElements = elements => {
   elements.forEach(element => {
+    hideElement(element);
+  });
+}
+const hideElement = element => {
     if (element.style) {
       element.style.opacity = 0;
       element.style.visibility = "hidden";
-    }
-  });
+  }
 }
-
 const showElements = elements => {
   elements.forEach(element => {
+    showElement(element);
+  });
+}
+const showElement = element => {
     if (element.style) {
       element.style.opacity = 1;
       element.style.visibility = "visible";
     }
-  });
 }
+
+
 
 let a = false;
 const observer = new MutationObserver(records => {
@@ -47,86 +54,124 @@ const observer = new MutationObserver(records => {
 
     console.log(`isActivate = ${configData["isActivate"]}`);
 
-    const parentNode = document.getElementById("dv-web-player");
-    if(parentNode){
-      HideProcess( configData , parentNode)
+    const baseNode1 = document.getElementById("dv-web-player");
+    if(baseNode1){
+      HideProcess( configData , baseNode1)
     }
 
-    const parentNode2 = document.getElementById("dv-web-player-2");
-    if(parentNode2){
-      HideProcess( configData , parentNode2)
+    const baseNode2 = document.getElementById("dv-web-player-2");
+    if(baseNode2){
+      HideProcess( configData , baseNode2)
     }
 
   });
 });
 
-const HideProcess = (configData , parentNode) => {
-    const parent = parentNode.querySelectorAll(".fq2lkuj");
-    const base1 = parent[0].querySelectorAll(".fkhz08q.f8hspre");
-    const base2 = parent[0].querySelectorAll(".ffszj3z.f8hspre.f1icw8u");
+const HideProcess = (configData, base) => {
+  const parent = base?.querySelector(".fq2lkuj");
+  const base1 = parent?.querySelector(".fkhz08q.f8hspre");
+  const base2 = parent?.querySelector(".ffszj3z.f8hspre.f1icw8u");
 
-    console.log(parent)
-    console.log(base1)
+
+
+  //翻訳設定のときだけ
+  const transraterSetting = parent?.querySelector(".fzvxfsy");
+  if (transraterSetting) {
+    //すでに下の位置位いるなら不要(初期位置に戻ってるはず)
+    console.log("transraterSetting");
+    const check1 = parent?.querySelector(".fkhz08q.f8hspre");
+    const check2 = parent?.querySelector(".ffszj3z.f8hspre.f1icw8u");
+    if (!(check1 && check2)) {
+      const aaa = parent?.parentNode?.querySelector(".fkhz08q.f8hspre");
+      const bbb = parent?.parentNode?.querySelector(".ffszj3z.f8hspre.f1icw8u");
+      if (aaa && bbb) {
+        transraterSetting.before(bbb);
+        transraterSetting.before(aaa);
+        showElement(parent)
+      }
+    }
+  } else {
     //並び替え
-    if (base1.length > 0 && base2.length > 0) {
-      console.log("InsertAdjacentHTML");
-      parent[0].after(base1[0]);
-      parent[0].after(base2[0]);
+    if (base1) {
+      parent.after(base1);
+    }
+    if (base2) {
+      parent.after(base2);
+    }
+    hideElement(parent);
+  }
+
+  //字幕位置と透過防止
+  const transraterText = parent?.parentNode?.querySelector(".fkhz08q.f8hspre")?.querySelector(".f35bt6a");
+  const transraterTextOpacity = transraterText?.querySelector(".atvwebplayersdk-captions-overlay");
+  const transraterTextPos = transraterText?.querySelector(".f1hy437");
+
+  if (transraterTextOpacity && transraterTextOpacity.style) {
+    console.log(transraterTextOpacity);
+    transraterTextOpacity.style.opacity = 1;
+  }
+
+    console.log(transraterTextPos);
+  if (transraterTextPos && transraterTextOpacity) {
+      transraterTextPos?.style.setProperty("height" , `99vh`);
+      transraterTextPos?.style.setProperty("margin-top" , `0vw`);
+  }else{
+      if(transraterTextOpacity) transraterTextOpacity.style.setProperty("height" , `0%`);
+      if(transraterTextOpacity) transraterTextOpacity.style.setProperty("margin-top" , `99vh`);
     }
 
-    const darkingElements = parentNode.querySelectorAll(".f8hspre.f1makowq");
-    const playStopButtons = parentNode.querySelectorAll(".f1aiijcp.fw80uk2");
-    const loadButton = parentNode.querySelectorAll(".f1dd7ldt");
-    const roadElements = parentNode.querySelectorAll(".f1la87wm");
-    const titles = parentNode.querySelectorAll(".f3w9jrr.fcckh95");
+  const darkingElements = base.querySelectorAll(".f8hspre.f1makowq");
+  const playStopButtons = base.querySelectorAll(".f1aiijcp.fw80uk2");
+  const loadButton = base.querySelectorAll(".f1dd7ldt");
+  const roadElements = base.querySelectorAll(".f1la87wm");
+  const titles = base.querySelectorAll(".f3w9jrr.fcckh95");
 
-    hideElements(parent);
 
-    if (!configData["isActivate"]) {
-      showElements(darkingElements);
-      showElements(playStopButtons);
-      showElements(roadElements);
-      showElements(titles);
-      return;
-    }
+  if (!configData["isActivate"]) {
+    showElements(darkingElements);
+    showElements(playStopButtons);
+    showElements(roadElements);
+    showElements(titles);
+    return;
+  }
 
-    if (configData["isPreventDarkening"]) {
-      hideElements(darkingElements);
-    } else {
-      showElements(darkingElements);
-    }
+  if (configData["isPreventDarkening"]) {
+    hideElements(darkingElements);
+  } else {
+    showElements(darkingElements);
+  }
 
-    if (configData["isHidePlaypauseButton"]) {
-      hideElements(playStopButtons);
-      hideElements(roadElements);
-      hideElements(loadButton);
+  if (configData["isHidePlaypauseButton"]) {
+    hideElements(playStopButtons);
+    hideElements(roadElements);
+    hideElements(loadButton);
 
-    } else {
-      showElements(playStopButtons);
-      showElements(roadElements);
-      showElements(loadButton);
-    }
+  } else {
+    showElements(playStopButtons);
+    showElements(roadElements);
+    showElements(loadButton);
+  }
 
-    if (configData["isHideTitle"]) {
-      hideElements(titles);
-    } else {
-      showElements(titles);
-    }
+  if (configData["isHideTitle"]) {
+    hideElements(titles);
+  } else {
+    showElements(titles);
+  }
 }
 
-var beforTarget = null;
+var beforeTarget = null;
 const waitLoadPage = () => {
   //なんとなく中央の再生ボタンが配置されてればページ読み込めているといるだろうという希望的観測
   const target = document.querySelector(".dv-player-fullscreen");
   //console.log(target);
-  if (target !== null && beforTarget !== target) {
+  if (target !== null && beforeTarget !== target) {
     observer.disconnect();
     console.log("Start CustomUI");
     observer.observe(target, {
       childList: true,
       subtree: true
     });
-    beforTarget = target;
+    beforeTarget = target;
     //clearInterval(intervalID);
   }
   //  else {
